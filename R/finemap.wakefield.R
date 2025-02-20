ffe9bacc1c965d2e0a4ca1f20f8f9099af9e9617
siwei.wu <wusiwei2022@163.com>
2025-02-20 11:35:39 +0000

    update wakerfield finemap

--- R/finemap.wakefield.R
+++ R/finemap.wakefield.R
@@ -1,8 +1,15 @@
 #' @export
-finemap.wakefield = function(data, chr = NULL, pos.lower = NULL, pos.upper = NULL, pthresh = 0.001, wake = 0.04, cs = 0.95){
-  if(is.null(chr) & is.null(pos.lower) & is.null(pos.upper)){message("Run Fine Mapping on the complete data")}else 
-    if(is.numeric(chr) & is.numeric(pos.lower) & is.numeric(pos.upper)){message("Run Fine Mapping on chromosome ", chr, "starting from ", pos.lower, " to ", pos.upper)}
-  else{stop("Please provide all of the chr, pos.lower, and pos.upper. Otherwise please provide set all of them as NULL to run on the complete data")}
+finemap.wakefield = function(data, chrom = NULL, pos.lower = NULL, pos.upper = NULL, pthresh = 0.0001, wake = 0.04, cs = 0.95){
+  if(is.null(chrom) & is.null(pos.lower) & is.null(pos.upper)){
+    message("Run Fine Mapping on the complete data")}else 
+    if(is.numeric(chrom) & is.numeric(pos.lower) & is.numeric(pos.upper)){
+      message("Run Fine Mapping on chromosome ", chrom, "starting from ", pos.lower, " to ", pos.upper)}
+  else{stop("Please provide all of the chrom, pos.lower, and pos.upper. Otherwise please provide set all of them as NULL to run on the complete data")
+       data = data %>% filter(chr = chrom & pos >= pos.lower & pos <= pos.upper)}
+  ## Check if the minimum p value < pthresh
+  if(min(data$p) > pthresh){
+    message(paste0("No variants in this genomic region reach p threshold of ", pthresh))
+    return(NULL)}
   ## Calculate Bayes Factors
   data = data %>% mutate(z = beta/se)
   ## Calculate variances
@@ -16,10 +23,10 @@ finemap.wakefield = function(data, chr = NULL, pos.lower = NULL, pos.upper = NUL
   data = data %>% mutate(postprob = abf2/abf2sum)
   ## Calculate the 95% Credible Set
   data = data[order(data$postprob, decreasing = TRUE), ]
-  data = data %>% mutate(credset = cumsum(postprob))
+  data = data %>% mutate(accum.postprob = cumsum(postprob))
   ## Output the result
   out = list()
   out[["Post.Prob"]] = data
-  out[["Cred.Set"]] = data %>% filter(credset < cs) %>% select("rsid")
+  out[["Cred.Set"]] = data[c(1, which(data$accum.postprob < cs) +1), ] %>% select("rsid")
   return(out)
 }
